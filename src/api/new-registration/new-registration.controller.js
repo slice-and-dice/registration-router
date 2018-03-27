7cba9411ad4706760469470b0bf2f73bc3774634
Joe Brady <joe.brady@uk.ibm.com>
2018-03-27 16:27:40 +0100

    Update response to JSON body format

--- src/api/new-registration/new-registration.controller.js
+++ src/api/new-registration/new-registration.controller.js
@@ -1,8 +1,12 @@
-const { routeMisConnectorService, routeNotifyService, routeFsaService } = require('../../services/');
-const store = require('../../store');
-const winston = require('winston');
+const {
+  routeMisConnectorService,
+  routeNotifyService,
+  routeFsaService
+} = require("../../services/");
+const store = require("../../store");
+const winston = require("winston");
 
-module.exports = async (data) => {
+module.exports = async data => {
   winston.info(`newregistration.controller called`);
 
   // TODO: use a new service to split the registration data into 'core registration fields' and 'enhanced registration fields'
@@ -13,31 +17,50 @@ module.exports = async (data) => {
     const destinationResults = [];
     const routePromises = [];
 
-    if(await store.getMasterRouting().then((masterRouting) => masterRouting.routeToLAs) === true) {
-      if(routing.emailConfig.active === true && routing.emailConfig.recipients.length >= 1) {
-        routePromises.push(routeNotifyService.send(registrationData, routing.emailConfig));
+    if (
+      (await store
+        .getMasterRouting()
+        .then(masterRouting => masterRouting.routeToLAs)) === true
+    ) {
+      if (
+        routing.emailConfig.active === true &&
+        routing.emailConfig.recipients.length >= 1
+      ) {
+        routePromises.push(
+          routeNotifyService.send(registrationData, routing.emailConfig)
+        );
       }
 
-      if(routing.misConfig.active === true) {
-        routePromises.push(routeMisConnectorService.send(registrationData, routing.misConfig));
+      if (routing.misConfig.active === true) {
+        routePromises.push(
+          routeMisConnectorService.send(registrationData, routing.misConfig)
+        );
       }
     }
 
-    if(await store.getMasterRouting().then((masterRouting) => masterRouting.routeToFSA) === true) {
-      if(routing.fsa === true) {
-        routePromises.push(routeFsaService.send(registrationData, data.localAuthority.id));
+    if (
+      (await store
+        .getMasterRouting()
+        .then(masterRouting => masterRouting.routeToFSA)) === true
+    ) {
+      if (routing.fsa === true) {
+        routePromises.push(
+          routeFsaService.send(registrationData, data.localAuthority.id)
+        );
       }
     }
 
-    return Promise.all(routePromises).then(responses => {
-      winston.info(`newregistration.controller successful`);
-      return responses;
-    }).catch(err => {
-      winston.error(`newregistration.controller failed: ${err}`);
-      return err;
-    });
-  }
-  catch (err) {
+    return Promise.all(routePromises)
+      .then(responses => {
+        winston.info(`newregistration.controller successful`);
+        const jsonResponse = { sentSuccessfully: responses };
+        return jsonResponse;
+      })
+      .catch(err => {
+        winston.error(`newregistration.controller failed: ${err}`);
+        return err;
+      });
+  } catch (err) {
     winston.error(`newregistration.controller failed: ${err}`);
     return err;
   }
