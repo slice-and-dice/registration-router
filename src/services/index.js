a89de2d1a27e623e665ca81b2aa2c6addf556730
Kiran Smith <kiran.smith@dwp.gsi.gov.uk>
2018-04-27 11:34:24 +0100

    removed format converter

--- src/services/index.js
+++ src/services/index.js
@@ -1,6 +1,5 @@
-const formatConverterService = require('./format-converter.service');
 const routeMisConnectorService = require('./route-mis-connector.service');
 const routeNotifyService = require('./route-notify.service');
 const routeFsaService = require('./route-fsa.service');
 
-module.exports = { formatConverterService, routeMisConnectorService, routeNotifyService, routeFsaService };
+module.exports = { routeMisConnectorService, routeNotifyService, routeFsaService };
